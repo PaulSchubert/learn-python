907a11193015ca8867d94a3da9488a30fd6c5666
Paul-Frederik Schubert <paul.frederik.schubert@gmail.com>
2022-04-15 21:56:23 -0700

    First commit, learning vector class in python

---
+++ vector/demo.py
@@ -0,0 +1,15 @@
+from Vector import Vector
+
+v1 = Vector(-2, 4)
+v2 = Vector(2, 1)
+v3 = v1 + v2
+v4 = v1 - v2
+v5 = v1 * 2
+
+print v1
+print v2
+print 'v1 + v2 = ', v3
+print 'v1 - v2 = ', v4
+print 'v1 * 2 = ', v5
+print 'v1 magnitude = ', abs(v1)
+
